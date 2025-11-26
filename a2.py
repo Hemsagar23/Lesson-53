dd7d6bd672d0cf0e0576f55f6058e1577ed13201
Hemsagar23 <kolluhemsagar@gmail.com>
2025-11-26 20:39:20 +0530

    initial commit

--- a2.py
+++ a2.py
@@ -5,7 +5,7 @@ root = Tk()
 root.geometry("200x200")
 
 def msg():
-    messagebox.showwarning("#^@#^ Getting Hacked %#@%#","Attention Please, You are being hacked.. pay 100$ immedietly to save your computer.")
+    messagebox.showwarning("#^@#^ Getting Hacked %#@%#","ATT3NT10N PL3AS3! Y0UR SYS7EM H4S B33N C0MPR0M1S3D... P4Y $100 1MM3D14T3LY T0 SAV3 Y0UR MACH1N3")
 
 button = Button(root, text="Scan for Virus", command=msg)
 button.place(x=40, y=80)
